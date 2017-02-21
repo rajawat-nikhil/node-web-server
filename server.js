17b2a139cbe68b1ac4212312958a5ffdc312c48a
nikhilrajawat <nikhilrajawatkota@gmail.com>
2017-02-21 11:33:02 +0530

    added the projects page

--- server.js
+++ server.js
@@ -25,6 +25,12 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/project', (req, res) => {
+  res.render('project.hbs'), {
+      pageTitle: "Project Profile Page"
+  }
+})
+
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
